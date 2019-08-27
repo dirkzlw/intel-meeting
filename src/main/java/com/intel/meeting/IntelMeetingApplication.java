d10a173807ab2f148c5372c351f5e5c3539d46a9
Ranger <dirkzh@163.com>
2019-08-27 15:24:41 +0800

    Initial commit

---
+++ src/main/java/com/intel/meeting/IntelMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.intel.meeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelMeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelMeetingApplication.class, args);
+    }
+
+}
