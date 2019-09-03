3d799db2cac05e3ce8a1de22a5a5ad0fcf1df3d6
intelme <2609542741@qq.com>
2019-09-03 16:45:38 +0800

    注册时获取验证码时对用户名和邮件是否存在的判断

--- src/main/java/com/intel/meeting/controller/UserController.java
+++ src/main/java/com/intel/meeting/controller/UserController.java
@@ -3,10 +3,10 @@ package com.intel.meeting.controller;
 import com.intel.meeting.po.Role;
 import com.intel.meeting.po.User;
 import com.intel.meeting.service.RoleService;
+import com.intel.meeting.service.UserService;
 import com.intel.meeting.vo.RtnIdInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +25,12 @@ public class UserController {
     @Autowired
     private RoleService roleService;
 
+    @Autowired
+    private UserService userService;
+    /**
+     * 跳转到查询用户界面
+     * @return
+     */
     @GetMapping("/to/usermgn/user-manage")
     public String toUserManage(Model model){
         List<Role> roleList = roleService.findAllRoles();
@@ -33,6 +39,33 @@ public class UserController {
         return "usermgn/user-manage";
     }
 
+    /**
+     * 跳转到登陆界面
+     * @return
+     */
+    @GetMapping("/to/user/login")
+    public String toUserLogin(){
+        return "user/login";
+    }
+
+    @PostMapping("/user/getcode")
+    @ResponseBody
+    public RtnIdInfo getVCode(String username,String email){
+        System.out.println("username = " + username);
+        System.out.println("email = " + email);
+        String result = userService.getVCode(username,email);
+        return new RtnIdInfo(result,0);
+    }
+
+
+
+
+
+    /**
+     * 管理员添加用户功能
+     * @param user
+     * @return
+     */
     @PostMapping("/usermgn/user/save")
     @ResponseBody
     public RtnIdInfo saveUser(User user){
